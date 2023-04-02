65ba4871dcb0223fd6471785021de518876c6a1c
Pauló Lajos <dev@paulolajos.hu>
2023-04-02 08:13:23 +0200

    02. TicTacToe - órai állapot

--- 02-TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ 02-TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tictactoe.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //Init view binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        //setContentView(R.layout.activity_main)
+
+        binding.btnReset.setOnClickListener {
+            binding.ticView.resetGame()
+        }
+    }
+
+    public fun showText(text: String) {
+        binding.tvData.text = text
     }
 }
--- 02-TicTacToe/app/src/main/java/com/example/tictactoe/view/TicTacToeView.kt
+++ 02-TicTacToe/app/src/main/java/com/example/tictactoe/view/TicTacToeView.kt
@@ -7,14 +7,13 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
+import com.example.tictactoe.MainActivity
+import com.example.tictactoe.model.TicTacToeModel
 
 class TicTacToeView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
     private var paintBackground: Paint = Paint()
     private var paintLine: Paint = Paint()
-
-    private var x: Float = -1f
-    private var y: Float = -1f
 
     init {
         paintBackground.color = Color.BLACK
@@ -29,17 +28,92 @@ class TicTacToeView(context: Context?, attrs: AttributeSet?) : View(context, att
         super.onDraw(canvas)
 
         canvas?.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paintBackground)
-        canvas?.drawLine(0f, 0f, width.toFloat(), height.toFloat(), paintLine)
-        canvas?.drawCircle(x, y, 50f, paintLine)
+        canvas?.let { drawGameArea(it) }
+        canvas?.let { drawPlayers(it) }
+
+    }
+
+    private fun drawGameArea(canvas: Canvas) {
+        // border
+        canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paintLine)
+        // two horizontal lines
+        canvas.drawLine(
+            0f, (height / 3).toFloat(), width.toFloat(), (height / 3).toFloat(),
+            paintLine
+        )
+        canvas.drawLine(
+            0f, (2 * height / 3).toFloat(), width.toFloat(),
+            (2 * height / 3).toFloat(), paintLine
+        )
+
+        // two vertical lines
+        canvas.drawLine(
+            (width / 3).toFloat(), 0f, (width / 3).toFloat(), height.toFloat(),
+            paintLine
+        )
+        canvas.drawLine(
+            (2 * width / 3).toFloat(), 0f, (2 * width / 3).toFloat(), height.toFloat(),
+            paintLine
+        )
+    }
+
+    private fun drawPlayers(canvas: Canvas) {
+        for (i in 0..2) {
+            for (j in 0..2) {
+                if (TicTacToeModel.getFieldContent(i, j) == TicTacToeModel.CIRCLE) {
+                    val centerX = (i * width / 3 + width / 6).toFloat()
+                    val centerY = (j * height / 3 + height / 6).toFloat()
+                    val radius = height / 6 - height / 20
+
+                    canvas.drawCircle(centerX, centerY, radius.toFloat(), paintLine)
+                } else if (TicTacToeModel.getFieldContent(i, j) == TicTacToeModel.CROSS) {
+                    canvas.drawLine(
+                        (i * width / 3 + width / 20).toFloat(),
+                        (j * height / 3 + height / 20).toFloat(),
+                        ((i + 1) * width / 3 - width / 20).toFloat(),
+                        ((j + 1) * height / 3 - height / 20).toFloat(), paintLine
+                    )
+
+                    canvas.drawLine(
+                        ((i + 1) * width / 3 - width / 20).toFloat(),
+                        (j * height / 3 + height / 20).toFloat(),
+                        (i * width / 3 + width / 20).toFloat(),
+                        ((j + 1) * height / 3 - height / 20).toFloat(), paintLine
+                    )
+                }
+            }
+        }
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        x = event.x
-        y = event.y
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val tX = event.x.toInt() / (width / 3)
+            val tY = event.y.toInt() / (height / 3)
+
+            if (tX < 3 && tY < 3 && TicTacToeModel.getFieldContent(tX, tY) == TicTacToeModel.EMPTY) {
+                TicTacToeModel.setFieldContent(tX, tY, TicTacToeModel.getNextPlayer())
+                TicTacToeModel.changeNextPlayer()
+
+                nextPlayer()
+            }
+        }
+
+        return super.onTouchEvent(event)
+    }
+
+    private fun nextPlayer() {
+        var next = "O"
+        if (TicTacToeModel.getNextPlayer() == TicTacToeModel.CROSS) {
+            next = "X"
+        }
+        (context as MainActivity).showText("Next player is: $next")
 
         invalidate()
+    }
 
-        //return super.onTouchEvent(event)
-        return true
+    public fun resetGame() {
+        TicTacToeModel.resetModel()
+        nextPlayer()
+        invalidate()
     }
 }
