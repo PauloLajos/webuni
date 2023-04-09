6b0dfa3de92323e22c74fa84bae50cf861f4442f
Pauló Lajos <dev@paulolajos.hu>
2023-04-09 11:26:23 +0200

    03. View példák - ImageView, ToggleButton, SpannableString

--- 03-LayoutDemo/app/src/main/java/com/example/layoutdemo/MainActivity.kt
+++ 03-LayoutDemo/app/src/main/java/com/example/layoutdemo/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.layoutdemo
 
+import android.graphics.Color
+import android.graphics.Typeface.BOLD
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import com.example.layoutdemo.databinding.ActivityMainBinding
+import com.example.layoutdemo.databinding.ActivityViewDemoBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityViewDemoBinding
+    //private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_scroll)
+        //setContentView(R.layout.activity_view_demo)
+
+
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityViewDemoBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val spannable = SpannableString("Text is spantastic!")
+        spannable.setSpan(
+            ForegroundColorSpan(Color.RED),
+            8, 12,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannable.setSpan(
+            StyleSpan(BOLD),
+            8, spannable.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.textView.text = spannable
     }
 }
