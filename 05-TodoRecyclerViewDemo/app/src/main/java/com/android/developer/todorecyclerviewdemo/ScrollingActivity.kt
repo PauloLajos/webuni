4b3310e4608cf8572340a699cc7362f58fe09e39
Pauló Lajos <dev@paulolajos.hu>
2023-04-23 07:02:35 +0200

    05. TodoRecyclerViewDemo - init

---
+++ 05-TodoRecyclerViewDemo/app/src/main/java/com/android/developer/todorecyclerviewdemo/ScrollingActivity.kt
@@ -0,0 +1,26 @@
+package com.android.developer.todorecyclerviewdemo
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.android.developer.todorecyclerviewdemo.databinding.ActivityScrollingBinding
+
+class ScrollingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScrollingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityScrollingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //setSupportActionBar(findViewById(R.id.toolbar))
+        setSupportActionBar(binding.toolbar)
+        binding.toolbarLayout.title = title
+    }
+}
