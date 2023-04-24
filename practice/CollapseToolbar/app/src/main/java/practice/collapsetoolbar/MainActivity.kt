101f03547aa62f5706bf37e41a3a0cb2e57e3406
Pauló Lajos <dev@paulolajos.hu>
2023-04-24 11:14:40 +0200

    Practice - CollapseToolBar add image

---
+++ practice/CollapseToolbar/app/src/main/java/practice/collapsetoolbar/MainActivity.kt
@@ -0,0 +1,19 @@
+package practice.collapsetoolbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import practice.collapsetoolbar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
