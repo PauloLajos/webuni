90e677b62e9ad75a54d4ee8696858ef07ab0851a
Pauló Lajos <dev@paulolajos.hu>
2023-04-30 20:10:27 +0200

    Practice - RoomDatabase: dependencies, viewBinding

---
+++ practice/RoomDatabase/app/src/main/java/practice/roomdatabase/MainActivity.kt
@@ -0,0 +1,19 @@
+package practice.roomdatabase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import practice.roomdatabase.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
