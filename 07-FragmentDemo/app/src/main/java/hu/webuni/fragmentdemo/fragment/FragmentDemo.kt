b6b41bbc36aaca990ab4b13a45bc5ab9ced0c8f9
Pauló Lajos <dev@paulolajos.hu>
2023-05-03 06:32:54 +0200

    07. FragmentDemo - viewBinding, fragment button setOnClickListener

--- 07-FragmentDemo/app/src/main/java/hu/webuni/fragmentdemo/fragment/FragmentDemo.kt
+++ 07-FragmentDemo/app/src/main/java/hu/webuni/fragmentdemo/fragment/FragmentDemo.kt
@@ -4,15 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import hu.webuni.fragmentdemo.R
+import hu.webuni.fragmentdemo.databinding.FragmentDemoBinding
 
 class FragmentDemo : Fragment() {
+
+    private var _binding: FragmentDemoBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_demo, container, false)
+
+        _binding = FragmentDemoBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+
+        _binding!!.btnDemo.setOnClickListener {
+            Toast.makeText(activity,"Demo",Toast.LENGTH_LONG).show()
+        }
+
+        return rootView
     }
 }
