599483fc22054adcf33ea8feaba9415801cc61b3
Pauló Lajos <dev@paulolajos.hu>
2023-05-07 08:41:44 +0200

    07. WeatherDemo - Házi feladat indul

---
+++ 07-WeatherDemo/app/src/main/java/hu/webuni/weatherdemo/MainActivity.kt
@@ -0,0 +1,20 @@
+package hu.webuni.weatherdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hu.webuni.weatherdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+        setContentView(view)
+
+
+    }
+}
